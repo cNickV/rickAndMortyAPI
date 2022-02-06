497af7190518ed856a65a2cfda6888ae135b68cf
Nicolas <nivemo035@hotmail.com>
2022-02-05 19:55:39 -0500

    creacion de la logica para utilizar dinamicanemte la API

--- app.js
+++ app.js
@@ -1,2 +1,38 @@
-const cards = document.getElementById("card-dinamic");
-const templateCard = document.getElementById("template-card").content;
+document.addEventListener("DOMContentLoaded", () => {
+  fetchData();
+});
+
+const fetchData = async () => {
+  try {
+    loadingData(true);
+
+    const res = await fetch("https://rickandmortyapi.com/api/character/");
+    const data = await res.json();
+    createCard(data);
+  } catch (err) {
+    console.error(err);
+  } finally {
+    loadingData(false);
+  }
+};
+
+const createCard = (data) => {
+  const cards = document.getElementById("card-dynamic");
+  const templateCard = document.getElementById("template-card").content;
+  const fragment = document.createDocumentFragment();
+
+  data.results.forEach((item) => {
+    const clone = templateCard.cloneNode(true);
+    clone.querySelector("h3").textContent = item.name;
+    fragment.appendChild(clone);
+  });
+};
+
+const loadingData = (state) => {
+  const loading = document.getElementById("loading");
+  if (state) {
+    loading.classList.remove("d-none");
+  } else {
+    loading.classList.add("d-none");
+  }
+};
